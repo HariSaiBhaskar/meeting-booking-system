08d3b1c72baf4afa5e388d3face761c6f2f2de4c
HariSaiBhaskar <arimilliharisaibhaskar@gmail.com>
2026-02-11 11:35:47 +0530

    Updated database config for Render deployment

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,21 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("meeting_booking", "postgres", "1234", {
-  host: "localhost",
-  dialect: "postgres",
-  logging: false,
-});
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: "postgres",
+      protocol: "postgres",
+      logging: false,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      }
+    })
+  : new Sequelize("meeting_booking", "postgres", "123", {
+      host: "localhost",
+      dialect: "postgres",
+      logging: false
+    });
 
 module.exports = sequelize;
