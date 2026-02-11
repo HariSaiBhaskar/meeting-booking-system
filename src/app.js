4dde2549f4a6e86d4861f05ab181c35b6af34aa8
HariSaiBhaskar <arimilliharisaibhaskar@gmail.com>
2026-02-11 14:41:35 +0530

    Fix: JWT auth, meeting conflict logic, route paths

--- src/app.js
+++ src/app.js
@@ -8,5 +8,10 @@ const meetingRoutes = require("./modules/meeting/routes/meeting.routes");
 
 app.use("/", userRoutes);        // /users and /login inside
 app.use("/meetings", meetingRoutes);
+app.get("/", (req, res) => {
+  res.send("Meeting Booking API is running 🚀");
+});
+
 
 module.exports = app;
+
